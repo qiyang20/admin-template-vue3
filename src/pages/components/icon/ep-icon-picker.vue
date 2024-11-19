a7ed70f5a4f3ef8f8ad32194e7a127a90c145351
qiyang20 <1079359761@qq.com>
2024-11-19 11:12:15 +0800

    fix:项目初始化

---
+++ src/pages/components/icon/ep-icon-picker.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-row>
+    支持所有Iconify sets：
+    <el-link href="https://icon-sets.iconify.design/">Iconify</el-link>
+  </el-row>
+  <div>直接使用icon="{collection}:{name}"的方式进行使用</div>
+  <span> icon: <Iconify :icon="iconRef" :style="style"></Iconify> </span>
+  <p></p>
+  <VpIconPicker @submit="handleSubmit"></VpIconPicker>
+</template>
+
+<script setup lang="ts">
+import type { VpIconPickerSubmitDataProps } from 'el-admin-components'
+import type { CSSProperties } from 'vue'
+
+definePage({
+  meta: {
+    title: 'pages.pick-icon',
+    icon: 'healthicons:i-exam-multiple-choice'
+  }
+})
+
+const iconRef = ref('ep:qian')
+const style = ref<CSSProperties>({})
+
+const handleSubmit = (data: VpIconPickerSubmitDataProps) => {
+  const { icon, fontSize, color } = data
+  iconRef.value = icon
+  style.value = {
+    fontSize: `${fontSize}px`,
+    color
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/pages/components/players/video.vue
@@ -0,0 +1,37 @@
+<template>
+  <VpVideoPlayer :options="options"></VpVideoPlayer>
+  <div class="pt-2">
+    <el-button type="primary" @click="handleClick">ChangeSource</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePage({
+  meta: {
+    title: 'components.video-player',
+    icon: 'ph:video'
+  }
+})
+
+import type { VpVideoPlayerOptions } from 'el-admin-components'
+
+const options = ref({
+  sources: [
+    {
+      src: 'https://toimc-online.static.toimc.com/vue-toimc-admin/video/001.mp4',
+      type: 'video/mp4'
+    }
+  ]
+} as VpVideoPlayerOptions)
+
+const handleClick = () => {
+  options.value.sources = [
+    {
+      src: 'https://toimc-online.static.toimc.com/vue-toimc-admin/video/002.mp4',
+      type: 'video/mp4'
+    }
+  ]
+}
+</script>
+
+<style scoped></style>
